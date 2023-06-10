356b3f340062c4e2eaee0e995d15e1a40b23b1b7
William Parsons <avolitty@gmail.com>
2023-06-10 11:25:20 -0700

    Deleting incorrect unsigned variants.

--- sorter.h
+++ sorter.h
@@ -1,52 +1,44 @@
 #ifndef SORTER_H
 #define SORTER_H
 
-void sorterA(unsigned long a, unsigned double * b);
+void sorterA(unsigned long a, unsigned long * b);
 
-void sorterB(unsigned long a, unsigned double * b);
+void sorterB(unsigned long a, unsigned long * b);
 
-void sorterC(unsigned long a, unsigned float * b);
+void sorterC(unsigned long a, unsigned int * b);
 
-void sorterD(unsigned long a, unsigned float * b);
+void sorterD(unsigned long a, unsigned int * b);
 
-void sorterE(unsigned long a, unsigned long * b);
+void sorterE(unsigned long a, unsigned short * b);
 
-void sorterF(unsigned long a, unsigned long * b);
+void sorterF(unsigned long a, unsigned short * b);
 
-void sorterG(unsigned long a, unsigned int * b);
+void sorterG(unsigned long a, unsigned char * b);
 
-void sorterH(unsigned long a, unsigned int * b);
+void sorterH(unsigned long a, unsigned char * b);
 
-void sorterI(unsigned long a, unsigned short * b);
+void sorterI(unsigned long a, double * b);
 
-void sorterJ(unsigned long a, unsigned short * b);
+void sorterJ(unsigned long a, double * b);
 
-void sorterK(unsigned long a, unsigned char * b);
+void sorterK(unsigned long a, float * b);
 
-void sorterL(unsigned long a, unsigned char * b);
+void sorterL(unsigned long a, float * b);
 
-void sorterM(unsigned long a, double * b);
+void sorterM(unsigned long a, long * b);
 
-void sorterN(unsigned long a, double * b);
+void sorterN(unsigned long a, long * b);
 
-void sorterO(unsigned long a, float * b);
+void sorterO(unsigned long a, int * b);
 
-void sorterP(unsigned long a, float * b);
+void sorterP(unsigned long a, int * b);
 
-void sorterQ(unsigned long a, long * b);
+void sorterQ(unsigned long a, short * b);
 
-void sorterR(unsigned long a, long * b);
+void sorterR(unsigned long a, short * b);
 
-void sorterS(unsigned long a, int * b);
+void sorterS(unsigned long a, char * b);
 
-void sorterT(unsigned long a, int * b);
-
-void sorterU(unsigned long a, short * b);
-
-void sorterV(unsigned long a, short * b);
-
-void sorterW(unsigned long a, char * b);
-
-void sorterX(unsigned long a, char * b);
+void sorterT(unsigned long a, char * b);
 
 #endif
