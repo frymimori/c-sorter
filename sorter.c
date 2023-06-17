6c5bd34139fdb272e4107939b7acb743a05086a6
William Parsons <avolitty@gmail.com>
2023-06-16 21:23:32 -0700

    Adding new descending sort order variant.

--- sorter.c
+++ sorter.c
@@ -174,28 +174,175 @@ void sorterA(unsigned long a, unsigned long * b) {
 }
 
 void sorterB(unsigned long a, unsigned long * b) {
-	unsigned long c = a - 1;
-	unsigned long d = 0;
+	unsigned long c = a >> 2;
+	unsigned long d;
 	unsigned long e;
 	unsigned long f;
+	unsigned long g;
+	unsigned long h;
+	unsigned long i;
 
-	while (a != 0) {
-		d = a;
-		e = a;
-		f = b[a];
-
-		while (d != 0) {
-			d--;
-
-			if (b[d] < f) {
-				f = b[d];
-				e = d;
+	if (a > 1) {
+		if (c > 100) {
+			c = 100;
+		} else {
+			if (c == 0) {
+				c = 1;
 			}
 		}
 
-		b[e] = b[a];
-		b[a] = f;
-		a--;
+		d = a % c;
+
+		if (((a / c) & 1) == 0) {
+			d += c;
+		}
+
+		e = 1;
+
+		while (d > e) {
+			f = a;
+			g = d;
+
+			while (e != g) {
+				f--;
+
+				if (b[f - 1] < b[f]) {
+					i = b[f - 1];
+					b[f - 1] = b[f];
+					b[f] = i;
+				}
+
+				g--;
+			}
+
+			if (b[f - 1] >= b[f]) {
+				e++;
+			}
+		}
+
+		e = a - d;
+		f = e;
+
+		while (f != 0) {
+			g = 1;
+
+			while (g != c) {
+				e = f;
+				h = c;
+
+				while (g != h) {
+					e--;
+
+					if (b[e - 1] < b[e]) {
+						i = b[e - 1];
+						b[e - 1] = b[e];
+						b[e] = i;
+					}
+
+					h--;
+				}
+
+				if (b[e - 1] >= b[e]) {
+					g++;
+				}
+			}
+
+			f -= c;
+		}
+
+		e = (a / c) - 1;
+
+		while (e != 0) {
+			e >>= 1;
+			f = 0;
+			g = e;
+
+			while (g != 0) {
+				f += c;
+				g--;
+
+				if (b[f - 1] < b[f]) {
+					while (b[f - 1] < b[f]) {
+						h = f - c;
+						i = b[f];
+						b[f] = b[f - 1];
+						f -= 1;
+
+						while (
+							f != h &&
+							b[f - 1] < i
+						) {
+							b[f] = b[f - 1];
+							f--;
+						}
+
+						b[f] = i;
+						h += c << 1;
+						f = h - c;
+						h--;
+						i = b[f];
+
+						while (
+							f != h &&
+							b[f + 1] > i
+						) {
+							b[f] = b[f + 1];
+							f++;
+						}
+
+						b[f] = i;
+						f = (h + 1) - c;
+					}
+				}
+
+				f += c;
+			}
+
+			if (
+				a != (d + f) ||
+				e == 0
+			) {
+				f += c;
+
+				if (b[f - 1] < b[f]) {
+					while (b[f - 1] < b[f]) {
+						h = f - c;
+						i = b[f];
+						b[f] = b[f - 1];
+						f -= 1;
+
+						while (
+							f != h &&
+							b[f - 1] < i
+						) {
+							b[f] = b[f - 1];
+							f--;
+						}
+
+						b[f] = i;
+						h += c + d;
+						f = h - d;
+						h--;
+						i = b[f];
+
+						while (
+							f != h &&
+							b[f + 1] > i
+						) {
+							b[f] = b[f + 1];
+							f++;
+						}
+
+						b[f] = i;
+						f = (h + 1) - d;
+					}
+				}
+
+				d += c;
+			}
+
+			c <<= 1;
+		}
 	}
 
 	return;
